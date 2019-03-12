4a88eed5d1384254baeb17bfdaf12b54b598b197
Sebastian Meier <sebastian.meier@5minds.de>
2019-03-12 17:04:39 +0100

    add connect hook

--- src/ihttp_socket_adapter.ts
+++ src/ihttp_socket_adapter.ts
@@ -1,7 +1,15 @@
-import {IEndpointSocketScope} from '@essential-projects/websocket_contracts';
+import {IEndpointSocketScope, OnConnectCallback} from '@essential-projects/websocket_contracts';
 import * as http from 'http';
 
 export interface IHttpSocketAdapter {
+
+  /**
+   * Executes the supplied callback each time a client connects to the socket.
+   *
+   * @param  {OnConnectCallback} callback
+   * @returns void
+   */
+  onConnect(callback: OnConnectCallback): void;
   initializeAdapter(httpServer: http.Server): Promise<any> | any;
   getNamespace(namespaceIdentifier: string): IEndpointSocketScope;
   dispose(): Promise<void> | void;
