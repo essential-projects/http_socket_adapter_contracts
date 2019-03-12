f7dc744e5ded962b52d0b6093f9193f9b4069ad4
Sebastian Meier <sebastian.meier@5minds.de>
2019-03-12 17:08:21 +0100

    add emit method

--- src/ihttp_socket_adapter.ts
+++ src/ihttp_socket_adapter.ts
@@ -10,6 +10,16 @@ export interface IHttpSocketAdapter {
    * @returns void
    */
   onConnect(callback: OnConnectCallback): void;
+
+  /**
+   * Sends a message using the supplied event type to all clients connected to
+   * the socket.
+   *
+   * @param  {string} eventType The event type used to send the message.
+   * @param  {TMessage} message The message to be sent.
+   * @returns void
+   */
+  emit<TMessage>(eventType: string, message: TMessage): void;
   initializeAdapter(httpServer: http.Server): Promise<any> | any;
   getNamespace(namespaceIdentifier: string): IEndpointSocketScope;
   dispose(): Promise<void> | void;
